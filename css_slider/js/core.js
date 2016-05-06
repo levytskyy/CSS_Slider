39e952b8e42ffc922884ded1ba2b334adfd41b0e
levytskyy <yuriy.levytskyy@gmail.com>
2016-05-06 17:11:48 +0300

    Added files via upload

---
+++ css_slider/js/core.js
@@ -0,0 +1,41 @@
+function slider(p){
+	var that = this,
+		pageNumber = 1,
+		animationTime = 600,
+		lastAnimation = 400;
+	this.id = 'bg__color';
+	this.numberSlide = $('#' + that.id + ' li').length;
+	
+	this.init = function() {
+		$(document).bind('mousewheel onwheel DOMMouseScroll MozMousePixelScroll', function(event) {
+			var timeNow = new Date().getTime();
+			if(timeNow - lastAnimation < 100 + animationTime) {
+				event.preventDefault();
+				return;
+			}
+			var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+			that.nextSlide(delta);
+			lastAnimation = timeNow;
+		});	
+		document.onmousemove = that.moveCursor;
+	},
+	this.nextSlide = function(delta) {
+		if(delta < 1){
+			if((pageNumber <= 1) || (pageNumber < this.numberSlide)){
+				pageNumber++;
+			}else{
+				pageNumber = 1;
+			}
+		}else{
+			if(pageNumber > 1){
+				pageNumber--;
+			}else{
+				pageNumber = this.numberSlide;
+			}
+		}
+		document.getElementById(this.id).setAttribute("data-active-page", pageNumber);	
+	}
+};
+
+var mainSlider = new slider();
+	mainSlider.init();
